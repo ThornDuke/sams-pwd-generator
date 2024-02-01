b983b4c5ae1ab4fe8036ddc64f9f7e6862638139
Sergio Spina <sergio.spina@spindox.it>
2024-02-01 20:43:04 +0100

    Consistency checks

--- src/globals.ts
+++ src/globals.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
  * if true many logs are generated and sent to the console
  */
 export const $$debugging = false;
-export const $$showErrors = true;
+export const $$logErrors = true;
 
 /**
  * Name of the key within the configuration file to look
